c49a52c2b8427e2669207a8d111dfc88e6f2b312
kalium <kalium222@outlook.com>
2026-06-30 02:48:52 -0400

    feat: list and get

--- worker.js
+++ worker.js
@@ -1,17 +1,53 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run "npm run dev" in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run "npm run deploy" to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
+// return the file on /path/to/file
+// list file on /music?prefix=...&limit=...
 export default {
-  async fetch(request, env, ctx) {
-    // You can view your logs in the Observability dashboard
-    console.info({ message: 'Hello World Worker received a request!' }); 
-    return new Response(`worker: \nrequest: ${request}\nenv: ${env}`);
-  }
+    async fetch(request, env, ctx) {
+        const url = new URL(request.url)
+        const path = url.pathname
+        console.log(path)
+        switch (request.method) {
+            case "GET": {
+                const prefix = url.searchParams.get('prefix') || ''
+                const limit = url.searchParams.get('limit') || 10
+                if (path === '/music')
+                    return await listFile(env.MUSIC, prefix, limit)
+                else {
+                    const key = decodeURIComponent(path.slice(1));
+                    console.log(key)
+                    return await getFile(env.MUSIC, key)
+                }
+            }
+            default:
+                return new Response("Method Not Allowed", {
+                    status: 405,
+                    headers: {
+                        Allow: "GET",
+                    },
+                })
+        }
+    }
 };
+
+async function listFile(bucket, prefix, limit) {
+    const r2objects = await bucket.list({ prefix, limit })
+    const keys = r2objects.objects.map(obj => obj.key)
+    const body = keys.join('\n')
+    const headers = new Headers()
+    return new Response(body, {
+        status: 200,
+        headers,
+    })
+}
+
+async function getFile(bucket, key) {
+    const r2object = await bucket.get(key)
+    if (r2object === null)
+        return new Response('Not Found', { status: 404 })
+    const headers = new Headers();
+    r2object.writeHttpMetadata(headers);
+    headers.set("etag", r2object.httpEtag);
+    return new Response(r2object.body, {
+        status: 200,
+        headers,
+    });
+}
